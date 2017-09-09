9a21034dfc3ff9a398a849d4bf88a54b8d51dc5d
rm <biffbangpow767@yahoo.com>
2017-09-08 17:50:48 -0700

    push a project experimenting with operators

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,20 +4,23 @@ class Mix
     @input1 = input1
     @input2 = input2
   end
-  def d_n_i
-    @dni = !include?(item)
-  end
   def check
     input_word1 = @input1
     input_word2 = @input2
-    dni = @dni
     arrays1 = []
     arrays2 = []
+    new_arrays = []
+    new_arrays.push(arrays1)
+    new_arrays.push(arrays2)
     arrays1.push(input_word1.upcase.split("").sort)
     arrays2.push(input_word2.upcase.split("").sort)
-    if arrays1.dni(["A", "E", "I", "O", "U", "Y"]) | arrays2.dni(["A", "E", "I", "O", "U", "Y"])
-      "You need to input actual words!"
-    elsif input_word1 === input_word2.reverse && input_word2 === input_word1.reverse
+    # new_arrays.each do |letter|
+    #   if letters.include?("A") | letters.include?("E") | letters.include?("I") | letters.include?("O") | letters.include?("U") | letters.include?("Y")
+    #     "Enter two words"
+    #   else "You need to input actual words!"
+    # end
+    # end
+    if input_word1 === input_word2.reverse && input_word2 === input_word1.reverse
        "This is an anagram & this is a palindrome!"
     elsif arrays1 === arrays2
        "This is an anagram!"
