ac1cfb4a2e30ed15e5e8335aaac135e972bd2adf
rm <biffbangpow767@yahoo.com>
2017-09-10 16:10:21 -0700

    commit a more organized project to complete the last three specs

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,27 +4,37 @@ class Mix
     @input1 = input1
     @input2 = input2
   end
-  def check
+  print "Enter two words"
+
+  def check()
     input_word1 = @input1
     input_word2 = @input2
+    input_letter1 = input_word1.upcase.split("").sort
+    input_letter2 = input_word2.upcase.split("").sort
+
     arrays1 = []
     arrays2 = []
-    new_arrays = []
-    new_arrays.push(arrays1)
-    new_arrays.push(arrays2)
-    arrays1.push(input_word1.upcase.split("").sort)
-    arrays2.push(input_word2.upcase.split("").sort)
-    # new_arrays.each do |letter|
-    #   if letters.include?("A") | letters.include?("E") | letters.include?("I") | letters.include?("O") | letters.include?("U") | letters.include?("Y")
-    #     "Enter two words"
-    #   else "You need to input actual words!"
-    # end
-    # end
+
+    arrays1.push(input_letter1)
+    arrays2.push(input_letter2)
+
     if input_word1 === input_word2.reverse && input_word2 === input_word1.reverse
        "This is an anagram & this is a palindrome!"
     elsif arrays1 === arrays2
        "This is an anagram!"
     else  "This is not an anagram nor palindrome!"
     end
+
+
+
+#     both_entered = input_letter1.concat(input_letter2)
+#     both_entered.each do |letter|
+#       arrays = []
+#     if letter.include?("A") | letter.include?("E") | letter.include?("I") | letter.include?("O") | letter.include?("U") | letter.include?("Y")
+#     arrays.push()
+#     return arrays
+#   else "Please enter a word!"
+#   end
+# end
   end
 end
