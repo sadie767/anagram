d10637ea561fd559c0969c103bbeb6b2963d7c6b
rm <biffbangpow767@yahoo.com>
2017-09-12 21:18:06 -0700

    commit a project with more practice using methods

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,35 +6,35 @@ class Mix
   end
   print "Enter two words"
 
-  def check()
-    input_word1 = @input1
-    input_word2 = @input2
-    input_letter1 = input_word1.upcase.split("").sort
-    input_letter2 = input_word2.upcase.split("").sort
+  def check
+    @input1 = input_letter1.upcase.delete(" ").split("").sort
+    @input2 = input_letter2.upcase.delete(" ").split("").sort
 
     arrays1 = []
     arrays2 = []
 
-    arrays1.push(input_letter1)
-    arrays2.push(input_letter2)
+    @arrays1 = arrays1.push(input_letter1)
+    @arrays2 = arrays2.push(input_letter2)
 
-    if input_word1 === input_word2.reverse && input_word2 === input_word1.reverse
+
+    if @input1 === @input2.reverse && @input1 === @input2.reverse
        "This is an anagram & this is a palindrome!"
-    elsif arrays1 === arrays2
+    elsif @arrays1 === @arrays2
        "This is an anagram!"
     else  "This is not an anagram nor palindrome!"
     end
-
-
-
-#     both_entered = input_letter1.concat(input_letter2)
-#     both_entered.each do |letter|
-#       arrays = []
-#     if letter.include?("A") | letter.include?("E") | letter.include?("I") | letter.include?("O") | letter.include?("U") | letter.include?("Y")
-#     arrays.push()
-#     return arrays
-#   else "Please enter a word!"
-#   end
-# end
   end
+
+  def vowel
+    @input1 = check_vowel1.upcase.split("")
+    @input2 = check_vowel2.upcase.split("")
+    both_entered = @input1.concat(@input2)
+
+    if @input1.include?("A") || @input1.include?("E") || @input1.include?("I") || @input1.include?("O") || @input1.include?("U") || @input1s.include?("Y") || @input1.include?("A") | @input1.include?("E") || @input2.include?("I") || @input2.include?("O") || @input2.include?("U") || @input2.include?("Y")
+        @input1.lowcase.join.check()
+        @input2..lowcase.join.check()
+    else "Please enter a real word!"
+    end
+  end
+
 end
