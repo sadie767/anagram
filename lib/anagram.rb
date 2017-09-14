a654eda442e51e57c54b45c75c44b458b0b50a49
rm <biffbangpow767@yahoo.com>
2017-09-13 22:10:12 -0700

    more experimenting

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,36 +5,27 @@ class Mix
     @input2 = input2.upcase.delete(" ", ".", ",").split("")
   end
 
-  @arrays1 = []
-  @arrays2 = []
-
-  # arrays1.sort()
-  # arrays2.sort()
-
-  # arrays1.push(@input1)
-  # arrays2.push(@input2)
-
-  def vowel
-    if @input1.include?("A") || @input1.include?("E") || @input1.include?("I") || @input1.include?("O") || @input1.include?("U") || @input1.include?("Y") ||
-    @input2.include?("A") || @input2.include?("E") || @input2.include?("I") || @input2.include?("O") || @input2.include?("U") || @input2.include?("Y")
-    @arrays1.push(@input1)
-    @arrays2.push(@input2)
-    else "Please enter a real word!"
-    end
-  end
-
-  @arrays1.sort()
-  @arrays2.sort()
-
   def check
+    arrays1 = []
+    arrays2 = []
+
+    arrays1.sort()
+    arrays2.sort()
+
     if @input1 === @input2.reverse && @input2 === @input1.reverse
+      # @input1.include?("A") || @input1.include?("E") || @input1.include?("I") || @input1.include?("O") || @input1.include?("U") || @input1.include?("Y") ||
+      # @input2.include?("A") || @input2.include?("E") || @input2.include?("I") || @input2.include?("O") || @input2.include?("U") || @input2.include?("Y")
+      return @input
        "This is an anagram & this is a palindrome!"
-    elsif @arrays1 === @arrays2
+    elsif arrays1 === arrays2 &&
+      @input1.include?("A") || @input1.include?("E") || @input1.include?("I") || @input1.include?("O") || @input1.include?("U") || @input1.include?("Y") ||
+      @input2.include?("A") || @input2.include?("E") || @input2.include?("I") || @input2.include?("O") || @input2.include?("U") || @input2.include?("Y")
        "This is an anagram!"
-       return @arrays1
-       return @arrays2
-    else  "This is not an anagram nor palindrome!"
+    elsif @input1.include?("A") || @input1.include?("E") ||     @input1.include?("I") || @input1.include?("O") || @input1.include?("U") || @input1.include?("Y") ||
+     @input2.include?("A") || @input2.include?("E") || @input2.include?("I") || @input2.include?("O") || @input2.include?("U") || @input2.include?("Y")
+       "This is not an anagram nor palindrome!"
+     else
+       "Please enter a real word!"
     end
   end
-
 end
