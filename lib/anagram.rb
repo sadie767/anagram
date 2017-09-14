ecbefb3bffdafadb14df9828df44d6dd9592a9f0
rm <biffbangpow767@yahoo.com>
2017-09-13 20:31:03 -0700

    more experimenting

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,41 +1,38 @@
 #!/usr/bin/env ruby
 class Mix
   def initialize(input1, input2)
-    @input1 = input1
-    @input2 = input2
+    @input1 = input1.upcase.delete(" ", ".", ",").split("")
+    @input2 = input2.upcase.delete(" ", ".", ",").split("")
   end
-  print "Enter two words"
+
+  arrays1 = []
+  arrays2 = []
+
+  arrays1.sort()
+  arrays2.sort()
+
+  arrays1.push(@input1)
+  arrays2.push(@input2)
+
+  vowel_arrays1 = []
+  vowel_arrays2 = []
+
+
+  def vowel
+    if @input1.include?("A") || @input1.include?("E") || @input1.include?("I") || @input1.include?("O") || @input1.include?("U") || @input1.include?("Y") ||
+    @input2.include?("A") || @input2.include?("E") || @input2.include?("I") || @input2.include?("O") || @input2.include?("U") || @input2.include?("Y")
+        @input1.check && @input2.check
+    else "Please enter a real word!"
+    end
+  end
 
   def check
-    letter1 = @input1
-    letter2 = @input2
-
-    letter1.upcase.split("")
-    letter2.upcase.split("")
-
-    arrays1 = []
-    arrays2 = []
-
-    arrays1.sort()
-    arrays2.sort()
-
-    trashes = []
-
-    arrays1.push(letter1)
-    arrays2.push(letter2)
-
-    if letter1 === letter2.reverse && letter2 === letter1.reverse
+    if @input1 === @input2.reverse && @input2 === @input1.reverse
        "This is an anagram & this is a palindrome!"
     elsif arrays1 === arrays2
        "This is an anagram!"
     else  "This is not an anagram nor palindrome!"
     end
-    return letter1
-    both_entered = arrays1.concat(arrays2)
-
-    if both_entered.include?("A") || both_entered.include?("E") || both_entered.include?("I") || both_entered.include?("O") || both_entered.include?("U") || both_entered.include?("Y")
-        trashes.push(both_entered)
-    else "Please enter a real word!"
-    end
   end
+
 end
