8fe233bb71c06e2b7b1b69800467a6d21416ec9f
rm <biffbangpow767@yahoo.com>
2017-09-16 08:35:22 -0700

    commit a finished file comparing anangrams, antigrams, and palindromes using Ruby

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,30 +5,32 @@ class Mix
     @input2 = input2
   end
 
-  def check
-    @input1.upcase.count('AEIOU')
-    @input2.upcase.count('AEIOU')
+  def vowels?
+    @input1.upcase.count('AEIOUY') === 0 || @input2.upcase.count('AEIOUY') === 0
+    "Please enter a real word!"
+  end
 
-    arrays1 = []
-    arrays2 = []
-    arrays1.push(@input1.split("").sort)
-    arrays2.push(@input2.split("").sort)
-    arrays1.delete(" ,.")
-    arrays2.delete(" ,.")
-    vowels = arrays1.count("AEIOU")
-    vowels2 = arrays2.count("AEIOU")
+  def palindromer
+    input1 = @input1.upcase.delete(' .,')
+    input2 = @input2.upcase.delete(' .,')
+    if input1 === input2.reverse
+      'This is a palindrome!'
+    end
+  end
 
+  def anagrammer
+    input1 = @input1.upcase.delete(' ,.').split('').sort
+    input2 = @input2.upcase.delete(' ,.').split('').sort
+    if input1 === input2
+      'This is an anagram!'
+    end
+  end
 
-    if @input1 == 0 | @input2 == 0
-      "Please enter a real word!"
-    elsif @input1 === @input2.reverse
-       "This is an anagram & this is a palindrome!"
-    elsif arrays1 === arrays2
-       "This is an anagram!"
-     elsif arrays1 != arrays2
-      "This is an antigram"
-    else
-      "This is neither an anagram nor palindrome"
+  def antigrammer
+    input1 = @input1.upcase.delete(' ,.')
+    input2 = @input2.upcase.delete(' ,.')
+    if input1 != input2
+      'This is an antigram!'
     end
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,22 +1,36 @@
 require "rspec"
 require "anagram"
 
-describe("#anagram") do
+describe("Anagram") do
+  describe("#check") do
 
-  it("check inputted words as anagrams") do
-    input = Mix.new("ruby", "bury")
-    expect(input.check).to(eq("This is an anagram!"))
-  end
-  it("compare anagrams regardless of capitalization") do
-    input1 = Mix.new("Hi", "ih")
-    expect(input1.check).to(eq("This is an anagram!"))
-  end
-  it("compare anagrams to decipher if they are palindromes") do
-    input2 = Mix.new("won", "now")
-    expect(input2.check).to(eq("This is an anagram & this is a palindrome!"))
-  end
-  it("tells user to input actual words") do
-    input3 = Mix.new("bbb", "now")
-    expect(input3.check).to(eq("Please enter a real word!"))
+    it("check inputted words as anagrams") do
+      input = Mix.new("ruby", "bury")
+      expect(input.anagrammer).to(eq("This is an anagram!"))
+    end
+    it("compares input regardless of capitalization") do
+      input1 = Mix.new("Hi", "ih")
+      expect(input1.anagrammer).to(eq("This is an anagram!"))
+    end
+    it("compares input to determine if they are palindromes") do
+      input2 = Mix.new("won", "now")
+      expect(input2.palindromer()).to(eq("This is a palindrome!"))
+    end
+    it("tells user to input actual words") do
+      input3 = Mix.new("bbb","saw")
+      expect(input3.vowels?()).to(eq("Please enter a real word!"))
+    end
+    it("compares input to determine if they are antigams") do
+      input4 = Mix.new("he", "was")
+      expect(input4.antigrammer()).to(eq("This is an antigram!"))
+    end
+    it("compares anagrams regardless of spaces or punctuation") do
+      input5 = Mix.new("bury.", "ru,by")
+      expect(input5.anagrammer()).to(eq("This is an anagram!"))
+    end
+    it("compares multiple words as anagrams or antigrams") do
+      input5 = Mix.new("was bury", "saw ruby")
+      expect(input5.anagrammer()).to(eq("This is an anagram!"))
+    end
   end
 end
